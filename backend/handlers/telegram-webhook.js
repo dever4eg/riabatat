0b4a431e55048ebaf066bd076b0e459bd31562b8
Bohdan Onyshchenko <Dever4eg@gmail.com>
2023-05-14 17:23:00 +0300

    Add dynamodb table (#5)
    * Add dynamodb table
    
    * Update primary key in DB

--- backend/handlers/telegram-webhook.js
+++ backend/handlers/telegram-webhook.js
@@ -1,13 +1,15 @@
 module.exports.handler = async (event) => {
-    return {
-      statusCode: 200,
-      body: JSON.stringify(
-        {
-          message: 'Update settings endpoint',
-          input: event,
-        },
-        null,
-        2
-      ),
-    };
+  console.log('telegram event', event)
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Update settings endpoint',
+        input: event,
+      },
+      null,
+      2
+    ),
   };
+};
