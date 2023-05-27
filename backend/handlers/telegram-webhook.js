e67b58cade341a7b8abb5a09528948c8b1083dcd
owner6 <owner1147@gmail.com>
2023-05-27 20:55:54 +0300

    finding errors in the code. Duplication of users

--- backend/handlers/telegram-webhook.js
+++ backend/handlers/telegram-webhook.js
@@ -1,13 +1,14 @@
-const { DynamoDBClient, PutItemCommand, GetItemCommand } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBClient, PutItemCommand, ScanCommand } = require("@aws-sdk/client-dynamodb");
 const client = new DynamoDBClient({ region: 'eu-central-1' });
+const { randomUUID } = require('crypto');
 
 module.exports.handler = async (event) => {
-  const { randomUUID } = require('crypto');
-  const uuid = randomUUID();
-console.log(uuid);
+  
   try {
+    const uuid = randomUUID();
+    
     const body = event.body;
-console.log(body);
+
     if (!body) {
       return {
         statusCode: 200
@@ -15,60 +16,58 @@ console.log(body);
     }
 
     const { message } = JSON.parse(body);
-    console.log(message);
-
-    let id;
-    let chatId;
-    let firstname;
-    let lastname;
-    let username;
-
-    if (message && message.text) {
-      const { chat } = message;
-      id = uuid;
-      chatId = chat.id;
-      firstname = chat.first_name || '';
-      lastname = chat.last_name || '';
-      username = chat.username || '';
-    }
+    
+    const { chat } = message || {};
+    const chatId = chat ? chat.id : undefined;
+    const firstname = chat ? chat.first_name || '' : '';
+    const lastname = chat ? chat.last_name || '' : '';
+    const username = chat ? chat.username || '' : '';
 
     // Проверка наличия пользователя в базе данных
-    const getItemCommand = new GetItemCommand({
-      Key: {
-        chatId: { N: chatId.toString() }
+    const command = new ScanCommand({
+      FilterExpression: 'chatId = :chatId',
+      ExpressionAttributeValues: {
+        ':chatId': { S: chatId.toString() },
       },
-      TableName: 'riabatat-dev-users',
+      ProjectionExpression: 'id, firstname, lastname, username',
+      TableName: 'riabatat-dev-users'
     });
-  
-    const getItemResponse = await client.send(getItemCommand);
-      console.log(getItemResponse);
-       
+
+    const response = await client.send(command);
+    
+    // Вывод имен пользователей
+    response.Items.forEach(function(item) {
+      console.log(item.id.S, item.firstname.S, item.lastname.S, item.username.S);
+    });
+    
+    const isUserExist = response.Items.length > 0;
+
     // Создание записи только если пользователь не существует
-    if (!getItemResponse.Item) {
-      const putCommand = new PutItemCommand({
+    if (!isUserExist) {
+      const putParams = {
         TableName: 'riabatat-dev-users',
         Item: {
           id: { S: uuid },
-          chatId: { N: chatId.toString() },
+          chatId: { S: chatId.toString() },
           // Проверка наличия и присваивание параметров
           ...(firstname && { firstname: { S: firstname } }),
           ...(lastname && { lastname: { S: lastname } }),
           ...(username && { username: { S: username } }),
-        },
-      });
-
-      // Execute PutItem command to save data in DynamoDB
+        }
+      };
+      
+      const putCommand = new PutItemCommand(putParams);
       await client.send(putCommand);
     }
-
+    
     return {
-      statusCode: 200,
+      statusCode: 200
     };
   } catch (error) {
     console.error('Error:', error);
 
     return {
-      statusCode: 200, 
+      statusCode: 200
     };
   }
 };
