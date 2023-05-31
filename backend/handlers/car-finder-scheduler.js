7df02539fd1c0850b84d78025944c6d83dec2d43
owner6 <owner1147@gmail.com>
2023-05-31 23:51:56 +0300

    Sorting each user and executing a request in Auto Ria

--- backend/handlers/car-finder-scheduler.js
+++ backend/handlers/car-finder-scheduler.js
@@ -9,80 +9,71 @@ module.exports.handler = async (event) => {
     // Получение списка пользователей из DynamoDB
     const scanCommand = new ScanCommand({ TableName: 'Users' });
     const scanResult = await client.send(scanCommand);
-    const users = scanResult.Items.map(item => item.username.S);
+    const users = scanResult.Items.map(item => ({
+      chatId: item.chatId.S,
+      searchSettings: item.searchSettings.S
+    }));
 
-    const brand = 'mazda';
-    const model = 'cx-5';
-    const year = '2020';
-    const apiKey = process.env.RIA_API_KEY;
+    console.log('Users:', users);
 
-    // Запит до API Autoria
-    const apiUrl = `https://api.auto.ria.com/brands/${brand}/models/${model}/years/${year}?api_key=${apiKey}`;
+    // Перебор каждого пользователя и выполнение запроса в AutoRia
+    for (const user of users) {
+      const { chatId, searchSettings } = user;
+      const { brand, model, year } = JSON.parse(searchSettings);
+      const apiKey = process.env.RIA_API_KEY;
 
-    // Виконання запиту до API Autoria
-    axios.get(apiUrl)
-      .then(response => {
-        const data = response.data;
-        const results = data.map((car) => {
-          return `Марка: ${car.brand}, Модель: ${car.model}, Рік випуску: ${car.year}`;
-        });
+      console.log(`Searching cars for user with chatId ${chatId}`);
+      console.log(`Search settings: brand=${brand}, model=${model}, year=${year}`);
 
-        // Функція відправки повідомлення в Telegram
-        function sendMessageTelegram(message, chatId, botToken) {
-          const telegramUrl = `https://api.telegram.org/bot${botToken}/sendMessage`;
-          
-          const options = {
-            url: telegramUrl,
-            method: 'POST',
-            json: true,
-            body: {
-              chat_id: chatId,
-              text: message,
-            },
-          };
-
-          request(options, (error, response, body) => {
-            if (!error && response.statusCode === 200) {
-              console.log('Повідомлення відправлено!');
-            } else {
-              console.error('Помилка при відправленні повідомлення:', error);
-            }
-          });
-        }
-      
-        // Використання функції для відправки повідомлення
-        const message = results.join('\n');
-        const chatId = '513983141';
-        const telegramApiKey = process.env.TELEGRAM_TOKEN;
-        sendMessageTelegram(message, chatId, telegramApiKey);
-      
-        return {
-          statusCode: 200,
-          body: JSON.stringify({
-            message: 'Scheduler',
-            results: results,
-            users: users,
-          }),
-        };
-      })
-      .catch(error => {
-        console.error('Помилка при отриманні даних з API Autoria:', error);
-        return {
-          statusCode: 500,
-          body: JSON.stringify({
-            message: 'Виникла помилка при отриманні даних',
-          }),
-        };
+      // Запрос к API AutoRia
+      const apiUrl = `https://api.auto.ria.com/brands/${brand}/models/${model}/years/${year}?api_key=${apiKey}`;
+      const response = await axios.get(apiUrl);
+      const data = response.data;
+      const results = data.map((car) => {
+        return `Марка: ${car.brand}, Модель: ${car.model}, Рік випуску: ${car.year}`;
       });
+
+      console.log(`Found ${results.length} cars for user with chatId ${chatId}`);
+
+      // Отправка уведомления в Telegram
+      function sendMessageTelegram(message, chatId, botToken) {
+        const telegramUrl = `https://api.telegram.org/bot${botToken}/sendMessage`;
+        
+        const options = {
+          url: telegramUrl,
+          method: 'POST',
+          json: true,
+          body: {
+            chat_id: chatId,
+            text: message,
+          },
+        };
+
+        request(options, (error, response, body) => {
+          if (!error && response.statusCode === 200) {
+            console.log('Повідомлення відправлено!');
+          } else {
+            console.error('Помилка при відправленні повідомлення:', error);
+          }
+        });
+      }
+    
+      // Использование функции для отправки уведомления
+      const message = results.join('\n');
+      const telegramApiKey = process.env.TELEGRAM_TOKEN;
+      sendMessageTelegram(message, chatId, telegramApiKey);
+    }
+
+    console.log('Finding cars');
+
+    return { statusCode: 200 };
   } catch (error) {
     console.error('Сталася помилка:', error);
     return {
       statusCode: 500,
       body: JSON.stringify({
-          message: 'Сталася помилка',		
-        }
-      ),	
+        message: 'Сталася помилка',		
+      }),
     }	
   }
 };
-       
