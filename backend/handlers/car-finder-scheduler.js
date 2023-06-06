4e54a840d9fc2449fd28f374c17d366dd18e5b07
owner6 <owner1147@gmail.com>
2023-06-06 16:52:34 +0300

    search for a list of cars according to the specified parameters

--- backend/handlers/car-finder-scheduler.js
+++ backend/handlers/car-finder-scheduler.js
@@ -6,12 +6,25 @@ const client = new DynamoDBClient({ region: 'eu-central-1' });
 module.exports.handler = async (event) => {
   try {
     // Assuming you have the `id_оголошення` value
-    const autoId = "32964882";
-    const apiKey = "#";
-    const url = `https://developers.ria.com/auto/info?api_key=${apiKey}&auto_id=${autoId}`;
+    const markaId = "9";
+    const modelId = "3219";
+    const sYers = "2010";
+    const poYers = "2016";
+    const apiKey = process.env.RIA_API_KEY;
+
+    // Build the API URL
+    const url = `https://developers.ria.com/auto/search`;
 
     // Make a GET request to the API
-    const response = await axios.get(url);
+    const response = await axios.get(url, {
+      params: {
+        api_key: apiKey,
+        'marka_id[0]': markaId,
+        'model_id[0]': modelId,
+        's_yers[0]': sYers,
+        'po_yers[0]': poYers
+      },
+    });
 
     // Assuming the API response contains car details
     const car = response.data;
@@ -19,6 +32,10 @@ module.exports.handler = async (event) => {
     // Print the car details
     console.log(car);
   } catch (error) {
-    console.error("Error:", error);
+    console.log('Error:', error.message);
+    if (error.response) {
+      console.log('responseData,', error.response.data);
+      console.log('response status',error.response.status);
+    } 
   }
 };
