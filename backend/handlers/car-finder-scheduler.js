c383e2dd44d5a32dd68163b9f43167b7fbeb7495
owner6 <owner1147@gmail.com>
2023-06-13 01:22:15 +0300

    Adding all machine ids to dynamoDB and checking for duplication

--- backend/handlers/car-finder-scheduler.js
+++ backend/handlers/car-finder-scheduler.js
@@ -1,5 +1,5 @@
 const axios = require("axios");
-const { DynamoDBClient, ScanCommand } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBClient, ScanCommand, UpdateItemCommand } = require("@aws-sdk/client-dynamodb");
 
 const client = new DynamoDBClient({ region: 'eu-central-1' });
 
@@ -7,32 +7,35 @@ module.exports.handler = async (event) => {
   try {
     // Получение telegramChatId из DynamoDB
     const scanCommand = new ScanCommand({
-      TableName: 'riabatat-dev-users', 
+      TableName: 'riabatat-dev-users',
     });
 
     const scanResult = await client.send(scanCommand);
     console.log('scanResult:', scanResult.Items);
-    
+
     const users = scanResult.Items.map(function(item) {
-      return { 
+      return {
         telegramChatId: item.telegramChatId.S,
         lastname: item.lastname.S,
         firstname: item.firstname.S,
         username: item.username.S,
-        id: item.id.S
+        id: item.id.S,
+        autoriaIdsUser: item.autoriaIdsUser ? item.autoriaIdsUser.SS : []
       };
     });
 
     console.log('users:', users);
+
+    const telegramChatIds = scanResult.Items.map((item) => item.telegramChatId.S);
     
-    const telegramChatId = scanResult.Items.map((item) => item.telegramChatId.S);
-    console.log('telegramChatId:', telegramChatId);
+    console.log('telegramChatIds:', telegramChatIds);
 
     // Assuming you have the `id_оголошення` value
     const markaId = "9";
     const modelId = "3219";
-    const sYers = "2014";
-    const poYers = "2014";
+    const sYers = "2010";
+    const poYers = "2023";
+    
     const apiKey = process.env.RIA_API_KEY;
 
     // Build the API URL
@@ -45,28 +48,49 @@ module.exports.handler = async (event) => {
         'marka_id[0]': markaId,
         'model_id[0]': modelId,
         's_yers[0]': sYers,
-        'po_yers[0]': poYers
+        'po_yers[0]': poYers,
+        'top': 9,
       },
     });
-    
+
     // Assuming the API response contains car details in a property called "cars"
     const cars = response.data.result.search_result.ids;
+    
     console.log('cars:', cars);
 
     // Send found cars to Telegram chat
-    for (const chatId of telegramChatId) {
+    for (const user of users) {
+      const { telegramChatId, id: userId, autoriaIdsUser } = user;
+      
       for (const id of cars) {
         const carLink = `https://auto.ria.com/uk/auto___${id}.html`;
-        const telegramMessage = `Found car: ${carLink}`;
+        
 
-        await axios.post(`https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/sendMessage`, {
-          chat_id: chatId,
-          text: telegramMessage
-        });
+        if (!autoriaIdsUser.includes(id)) {
+          const telegramMessage = `Found car: ${carLink}`;
+          
+          await axios.post(`https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/sendMessage`, {
+            chat_id: telegramChatId,
+            text: telegramMessage
+          }); 
+
+          //обновляем запись пользователя в dynamoDB, добавляя новую ссылку в список
+          const updateItemCommand = new UpdateItemCommand({
+            TableName: 'riabatat-dev-users',
+            Key: {
+              id: { S: userId },
+            },
+            UpdateExpression: "ADD autoriaIdsUser :id",
+            ExpressionAttributeValues: {
+              ":id": { SS: [id] }
+            }
+          });
+          await client.send(updateItemCommand);
+        }
       }
     }
-    } catch (error) {
-      console.log('Error:', error.message);
+  } catch (error) {
+    console.log('Error:', error.message);
     if (error.response) {
       console.log('responseData,', error.response.data);
       console.log('response status', error.response.status);
