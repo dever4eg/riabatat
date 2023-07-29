e407efa51a4a93cf7de021d6ca1990aec66583ed
owner6 <owner1147@gmail.com>
2023-07-29 13:54:59 +0300

    component vue in the edit structure template

--- settings-page/src/App.vue
+++ settings-page/src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <dev>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </dev>
 </template>
 
 <script>
